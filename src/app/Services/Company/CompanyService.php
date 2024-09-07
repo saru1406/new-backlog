cefbafe0dd10eaad2dda8557e259eb8e8d0ffba8
yuta-saruwatari <y.s1406@ezweb.ne.jp>
2024-09-07 13:51:06 +0900

    declare追加

--- src/app/Services/Company/CompanyService.php
+++ src/app/Services/Company/CompanyService.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services\Company;
 
 class CompanyService implements CompanyServiceInterface
--- src/app/Usecase/Company/StoreCompanyUsecase.php
+++ src/app/Usecase/Company/StoreCompanyUsecase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Usecase\Company;
 
 use App\Exceptions\AlreadyExistsException;
