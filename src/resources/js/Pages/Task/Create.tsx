18ee1a4c2927703af5835d0a1ab949cbe3bcd81c
yuta-saruwatari <y.s1406@ezweb.ne.jp>
2024-09-17 05:47:14 +0900

    datePicker追加

--- src/resources/js/Pages/Task/Create.tsx
+++ src/resources/js/Pages/Task/Create.tsx
@@ -4,11 +4,16 @@ import ProjectLayout from '@/Layouts/ProjectLayout';
 import { PageProps } from '@/types';
 import { Project } from '@/types/project';
 import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ja } from 'date-fns/locale';
 
 export default function TaskCreate({
     project,
 }: PageProps & { project: Project }) {
-    const [selected, setSelected] = useState<Date>();
+    const [selectedStartDate, setSelectedStartDate] = useState<Date | null>(null);
+    const [selectedEndDate, setSelectedEndDate] = useState<Date | null>(null);
+
     return (
         <ProjectLayout
             project={project}
@@ -135,44 +140,35 @@ export default function TaskCreate({
                 <div className='flex my-5'>
                     <div className='w-full flex mr-7'>
                         <label
-                            htmlFor='priority'
+                            htmlFor='start_date'
                             className='font-medium text-sm text-gray-700 w-1/3 flex items-center'
                         >
                             開始日
                         </label>
-                        <select
-                            name='priority'
-                            id='priority'
-                            className='rounded-md border-gray-300 shadow-sm min-w-48 max-w-60 text-sm'
-                        >
-                            <option value='' disabled selected>
-                                選択してください
-                            </option>
-                            <option value='1'>低</option>
-                            <option value='2'>中</option>
-                            <option value='3'>高</option>
-                        </select>
+                        <DatePicker
+                            selected={selectedStartDate}
+                            onChange={(date) => setSelectedStartDate(date)}
+                            locale={ja}
+                            placeholderText='日付を選択してください'
+                            dateFormat='yyyy/MM/dd'
+                            className='p-2 border border-gray-300 rounded-md shadow-sm'
+                        />
                     </div>
                     <div className='w-full flex'>
                         <label
-                            htmlFor='version'
+                            htmlFor='end_date'
                             className='font-medium text-sm text-gray-700 flex items-center w-1/3'
                         >
                             期限日
                         </label>
-                        <select
-                            name='version'
-                            id='version'
-                            className='rounded-md border-gray-300 shadow-sm min-w-48 max-w-60 text-sm'
-                        >
-                            <option value='' disabled selected>
-                                選択してください
-                            </option>
-                            <option value='1'>要件定義</option>
-                            <option value='2'>設計</option>
-                            <option value='3'>開発</option>
-                            <option value='4'>テスト</option>
-                        </select>
+                        <DatePicker
+                            selected={selectedEndDate}
+                            onChange={(date) => setSelectedEndDate(date)}
+                            locale={ja}
+                            placeholderText='日付を選択してください'
+                            dateFormat='yyyy/MM/dd'
+                            className='p-2 border border-gray-300 rounded-md shadow-sm'
+                        />
                     </div>
                 </div>
                 <hr />
